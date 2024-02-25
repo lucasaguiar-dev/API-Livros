b29a68bb505bc8e34f03b687b7312ea84c032f6a
Lucas Aguiar <lucas.9526@hotmail.com>
2024-02-25 17:29:27 -0300

    Create inital setup

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
-print("hello world!") 
-print('testando 1 commit')
-print('Estou editando') 
+from flask import Flask, request
+# from flasgger import Swagger, swag_from
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("url_postgresql")
+db = SQLAlchemy(app)
+
+class Livro(db.model):
+
+
+@app.route("/")
+def home():
+    return ("hello world")
+
+if __name__ == "__main__":
+    app.run(debug=True)
